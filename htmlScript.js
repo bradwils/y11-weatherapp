8f9ef4903e6c4f709f9c0ed8d0c9c1174b387b1e
brad <115906450+bWils0@users.noreply.github.com>
2023-03-07 22:25:52 +1100

    made some stuff work, added some more files, it's all for testing tho
    removed apiscript
    
    made htmlscript work (mostly), api query now works so that's a good start
    
    notes.txt for some general notes
    
    spareScript for temp use and/or testing (for nodejs purposes since i like using it)
    
    test.html is the webpage i'm using to make sure that everything is correctly displaying thru html, incase i test something in nodejs (spareScript), and the feature doesn't work as expected in html.

---
+++ htmlScript.js
@@ -0,0 +1,42 @@
+var text1 = document.getElementById('text1').value //html
+//above doesn't work. dunno why. fix later., it's unimportant rn.
+
+var response;
+var data;
+
+
+{ //shortcut vars
+var localName;
+var currentWeather;
+var description;
+var temp;
+var feelslike;
+var windSpeed;
+var localTime;
+var humidityPercent;
+var coordinates; //maybe if i have time at the end, show the location on a map? would be cool.
+}
+
+
+var response = '';
+var data = '';
+var url = 'https://api.openweathermap.org/data/2.5/weather?q=marrickville&appid=de4315905984c51cb8f1bb4c23c949c0&units=metric';
+
+async function APIUpdate() { //needs search functionality
+    console.log('APIUpdate started')
+    response = await fetch(url);  //fetchs the info from api
+    data = await response.json() //parses it into a json format in the code
+    console.log(data.weather[0].description)
+    setAPIShortcuts()
+}
+
+function setAPIShortcuts() {
+
+
+    console.log('data + weather[0].description: ' + data.weather[0].description) //this one works
+    text1 = 'current conditions in sydney: ' + data.weather[0].description //html
+}
+
+
+
+//status:
---
+++ spareScript.js
@@ -0,0 +1,13 @@
+//remove any html elements (document.getElementByid, etc), so it can be run in nodeJS
+
+//       _   _  ____  _____  ______     _  _____    __  _   _  ____    _    _ _______ __  __ _       __     _____  _____ _____  _____ _____ _______ 
+//      | \ | |/ __ \|  __ \|  ____|   | |/ ____|  / / | \ | |/ __ \  | |  | |__   __|  \/  | |      \ \   / ____|/ ____|  __ \|_   _|  __ \__   __|
+//      |  \| | |  | | |  | | |__      | | (___   | |  |  \| | |  | | | |__| |  | |  | \  / | |       | | | (___ | |    | |__) | | | | |__) | | |   
+//      | . ` | |  | | |  | |  __| _   | |\___ \  | |  | . ` | |  | | |  __  |  | |  | |\/| | |       | |  \___ \| |    |  _  /  | | |  ___/  | |   
+//      | |\  | |__| | |__| | |___| |__| |____) | | |  | |\  | |__| | | |  | |  | |  | |  | | |____   | |  ____) | |____| | \ \ _| |_| |      | |   
+//      |_| \_|\____/|_____/|______\____/|_____/  | |  |_| \_|\____/  |_|  |_|  |_|  |_|  |_|______|  | | |_____/ \_____|_|  \_\_____|_|      |_|   
+//                                                 \_\                                               /_/                                            
+                                                                                                                                            
+
+//everytime you need to run it in node (better for debugging & testing stuff), paste it here and strip html elemements (rely on console logs). makes some stuff easier, and brad (myself) prefers it for dev'ing.
+//this is temp. brad likes it for testing/debugging. not for general use.
