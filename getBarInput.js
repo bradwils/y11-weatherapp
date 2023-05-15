f345a5bc56fd53c55b1d5661deb0b5860e8314f0
xXJoeljsXx <126838973+xXJoeljsXx@users.noreply.github.com>
2023-05-15 10:18:44 +1000

    recent tab stuff

--- getBarInput.js
+++ getBarInput.js
@@ -3,9 +3,18 @@
 
 var cmds = ['help', 'size', 'update canvas', 'stylewidth', 'newframe', "width (don't bother with it rn)" ]
 var barInput;
+var recent = [];
+var recentInput;
+var recentDisplay = document.getElementById("recentResult");
+
+function clearRecent() {
+    document.getElementById("recentResult").disabled = true;
+    for (i = 0; i < 10; i++) {
+        recent[i] = "";
+    }
+}
 
 function debugGetBar() {
-
     barInput = (document.getElementById('searchbar').value).toLowerCase();
     splitInput = barInput.split(' ');
     console.log('barInput: ' + barInput)
@@ -42,7 +51,32 @@ function debugGetBar() {
         console.log('APIUpdate ran')
     }
 
+    recentInput = document.getElementById("searchbar").value;
+
     document.getElementById('searchbar').value = '';
+    
+    recent[9] = recent[8];
+    recent[8] = recent[7];
+    recent[7] = recent[6];
+    recent[6] = recent[5];
+    recent[5] = recent[4];
+    recent[4] = recent[3];
+    recent[3] = recent[2];
+    recent[2] = recent[1];
+    recent[1] = recent[0];
+    recent[0] = recentInput;
+
+    recentDisplay.innerHTML = "Recent searches ----> " +
+    "\n" + " " + recent[0] +
+    "\n" + " " + recent[1] +
+    "\n" + " " + recent[2] +
+    "\n" + " " + recent[3] +
+    "\n" + " " + recent[4] +
+    "\n" + " " + recent[5] +
+    "\n" + " " + recent[6] +
+    "\n" + " " + recent[7] +
+    "\n" + " " + recent[8] +
+    "\n" + " " + recent[9];
 }
 
 
